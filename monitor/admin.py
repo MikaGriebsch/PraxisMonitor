5cb8855cc267e6404c417db658f0b997f6ba32c3
Mika Griebsch <mika.griebsch@gmail.com>
2025-03-19 15:56:26 +0100

    Such Zeile eingefügt für Personen

--- monitor/admin.py
+++ monitor/admin.py
@@ -4,7 +4,8 @@ from .models import *
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "status")
-    list_filter = ("status",)
+    search_fields = ("first_name", "last_name")
+    #list_filter = ("status",)
     list_editable = ("status",)
     change_list_template = 'admin/patient_change_list.html'
 
@@ -14,7 +15,6 @@ class PatientAdmin(admin.ModelAdmin):
             'waiting': Patient.objects.filter(status='waiting'),
             'room1': Patient.objects.filter(status='room1'),
             'room2': Patient.objects.filter(status='room2'),
-            #'in_treatment': Patient.objects.filter(status='in_treatment'),
             'done': Patient.objects.filter(status='done'),
         })
         return super().changelist_view(request, extra_context=extra_context)
